d2ea313c9fb912180ec98fcc505604a24cd6763c
gurisinfo <csgurisinfo@gmail.com>
2022-01-04 17:24:25 +0700

    Slicing login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="{{ url('/') }}">
                 <img src="{{ url('assets/images/logo.png') }}" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,10 +43,10 @@
                     </li>
                 </ul>
                 <div class="d-flex">
-                    <a href="#" class="btn btn-master btn-secondary me-3">
+                    <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3">
                         Sign In
                     </a>
-                    <a href="#" class="btn btn-master btn-primary">
+                    <a href="{{ route('login') }}" class="btn btn-master btn-primary">
                         Sign Up
                     </a>
                 </div>
