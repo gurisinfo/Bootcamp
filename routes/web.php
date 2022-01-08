f66e02f4de8e7e44081a59f018f0e6015f58a3da
gurisinfo <csgurisinfo@gmail.com>
2022-01-08 16:23:39 +0700

    Integration Admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\User\CheckoutController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,16 +20,36 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
-Route::get('/checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
-Route::post('/checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
-
 //socialite route
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    // checkout routes
+    Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('/checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('/checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    // dashboard
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    
+    // user dashboard
+    // Route::prefix('user/dashboard')->namespace('User')->name('user.')->middleware('ensureUserRole:user')->group(function(){
+        // Route::get('/', [UserDashboard::class, 'index'])->name('dashboard');
+    // });
+
+    // admin dashboard
+    // Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('ensureUserRole:admin')->group(function(){
+        // Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
+
+        // admin checkout
+        // Route::post('checkout/{checkout}', [AdminCheckout::class, 'update'])->name('checkout.update');
+    // });
+
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
